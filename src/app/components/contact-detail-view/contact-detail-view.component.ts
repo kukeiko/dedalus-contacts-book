528a0bfc40c347a9c29df0f7bd88023556afbade
Magna Marek <magna.marek@gmail.com>
2024-06-16 16:35:41 +0200

    feat/fix/perf(contact-detail-view): remove "select contact" notice / properly apply padding / use OnPush

--- src/app/components/contact-detail-view/contact-detail-view.component.ts
+++ src/app/components/contact-detail-view/contact-detail-view.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { Contact } from "../../models";
 
 @Component({
     selector: "contact-detail-view",
     templateUrl: "./contact-detail-view.component.html",
     styleUrl: "./contact-detail-view.component.scss",
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ContactDetailViewComponent {
     @Input() contact?: Contact;
